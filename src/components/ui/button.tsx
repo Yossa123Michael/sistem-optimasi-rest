22b88842a24aef6a1e716b01bc639337314262c4
Yossa123Michael <122776691+Yossa123Michael@users.noreply.github.com>
2025-12-06 22:54:46 +0000

    Edited Spark

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -50,10 +50,15 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(
+        buttonVariants({ variant, size, className }),
+        "font-light",
+        "font-thin",
+        "text-xs"
+      )}
       {...props}
     />
-  )
+  );
 }
 
 export { Button, buttonVariants }
